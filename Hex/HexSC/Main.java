99741b6c6a6f4ebc5d2e1924302ab7a974bf0c9b
Souhail louzi <souhail.studying@gmail.com>
2024-02-09 18:00:14 +0100

    first push

---
+++ Hex/HexSC/Main.java
@@ -0,0 +1,19 @@
+package HexSC;
+/*
+ * The Hex game (https://en.wikipedia.org/wiki/Hex_(board_game)) is a simple beard that have two players each one have a Theoretically
+ * infinite amount of Tokens each player have Tokens of a color either white or black
+ * the starter of the game i chosen randomly and given the black Tokens.
+ * The board can be a 11x11 or 13x13 or 19x19 Rhombus (check https://en.wikipedia.org/wiki/Rhombus
+ * for more info) .
+ * Player take turns putting there tokens in the beard as long as the spot is empty .
+ * The game ends when a player make a connection from the end of the beard facing him
+ * to the end facing the opponent.
+ * this game is solved (meaning the starting position can always win) hence the existence
+ * of an AI or algorithm that can never be beaten given the first move , and that can present great 
+ * challenge even when he got put in second creating such bot is the object of the second part of the
+ * project.
+ * 
+ */
+public class Main {
+
+}
